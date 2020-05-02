0941bbe665dfbe39c0c42b7190e20119ba9f15ad
Enrique Taietta <e.taietta@happenin.it>
2020-05-02 16:00:34 +0200

    Added watchQuery property for test

--- pages/index.vue
+++ pages/index.vue
@@ -50,6 +50,7 @@ import FooterBlue from '../components/FooterBlue.vue'
 import TaskBoardList from '../components/TaskBoardList.vue'
 
 export default {
+  watchQuery: [],
   components: {
     TaskBoardList,
     HeaderBlue,
